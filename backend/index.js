875fa626932d30a41e6effa5e76f920d8f0980a5
kunal-216 <kunaltalreja216@gmail.com>
2024-07-17 04:03:55 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use('/api/v1/ai', aiRoutes);
 
 app.get('/', async (req, res) => {
   res.status(200).json({
-    message: 'Hello from DALL.E!',
+    message: 'Hello from Artist!',
   });
 });
 
